b82b57c91539f9b5a9510a020136e04275b83e51
Joseph Rai <oxstone7@gmail.com>
2026-02-25 13:37:53 +0900

    ch03

---
+++ ch03/sigmoid.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x))
+
+x = np.array([-1.0, 1.0, 2.0])
+print(sigmoid(x))
+
+t = np.array([1.0, 2.0, 3.0])
+print(1.0 + t)
+print(1.0 / t)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1) # y축의 범위 지정
+plt.show()
